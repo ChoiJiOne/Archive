f01b18ab8a91ba80c4af210b817d96ef9e79c396
CHOIJIONE\choij <choijione02@gmail.com>
2023-07-09 09:37:13 +0900

    Tutorial03 실습 코드 추가

---
+++ Tutorial03/Source/GraphicsClass.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <windows.h>
+
+const bool FULL_SCREEN = false;
+const bool VSYNC_ENABLED = true;
+const float SCREEN_DEPTH = 1000.0f;
+const float SCREEN_NEAR = 0.1f;
+
+class D3DClass;
+
+class GraphicsClass
+{
+public:
+	GraphicsClass();
+	GraphicsClass(const GraphicsClass& other);
+	virtual ~GraphicsClass();
+
+	bool Initialize(int screenWidth, int screenHeight, HWND hwnd);
+	void Shutdown();
+	bool Frame();
+
+private:
+	bool Render();
+
+private:
+	D3DClass* m_Direct3D = nullptr;
+};
